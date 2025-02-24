7ad5fe8c3451f32da610aab2fc3f7e6e74f1f02b
GrandLay-e <sow.abdallh@gmail.com>
2025-02-24 10:34:40 +0100

    PlusOuMoins

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace mSoftLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenue au jeu de devinette !");
+            Random random = new Random();
+            int hazard = random.Next(1,1000);
+            int lastPos = 1000;
+            int lastNeg = 1;
+            int cherche = lastNeg;
+
+            int coups = 0;
+            do{
+                if(cherche < hazard)
+                {
+                    Console.WriteLine("+");
+                    lastNeg = cherche;
+                    cherche = random.Next(lastNeg, lastPos);
+                }
+                else if(cherche > hazard)
+                {
+                    Console.WriteLine("-");
+                    lastPos = cherche;
+                    cherche = random.Next(lastNeg, lastPos);
+                }
+                coups ++;
+                Console.WriteLine($"[ {lastNeg} \t| {cherche} <--> {hazard} \t| {lastPos} ]");
+            }while(cherche != hazard);
+            Console.WriteLine("\n\tGagné ! "+coups+" coups ");
+        }
+    }
+}
